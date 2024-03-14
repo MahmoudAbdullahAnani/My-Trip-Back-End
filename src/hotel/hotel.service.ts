a499c4838cb610b2cc268a4e7284ae523510dba0
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-03-14 15:00:31 +0200

    add search hotels

--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -18,7 +18,7 @@ export class HotelService {
     const storeDateOutY = `${dateReturnState}`.split('/')[2];
     const storeDateOutMonth = `${dateReturnState}`.split('/')[1];
     const storeDateOutDay = `${dateReturnState}`.split('/')[0];
-    const api = `https://serpapi.com/search.json?engine=google_hotels&q=${chooseCityNameHotel}&check_in_date=${storeDateGoY}-${storeDateGoMonth}-${storeDateGoDay}&check_out_date=${storeDateOutY}-${storeDateOutMonth}-${storeDateOutDay}&adults=${adultsDataState}&currency=EGP&gl=ar&hl=ar&api_key=${process.env.SERPAPI_KEY}`;
+    const api = `https://serpapi.com/search.json?engine=google_hotels&q=${chooseCityNameHotel}&check_in_date=${storeDateGoY}-${storeDateGoMonth}-${storeDateGoDay}&check_out_date=${storeDateOutY}-${storeDateOutMonth}-${storeDateOutDay}&adults=${adultsDataState}&currency=EGP&gl=ar&hl=ar&api_key=3003862109edfb689c88c5d6b7b552f32f0cac58d9ca4b8a9e608f556eb412f9`;
     return await axios
       .get(api)
       .then(({ data }) => {
